2c707dd144e5bfd911007b0c040ebfce0edec4d0
Bhuvanesh <bhuvanesh.sel@gmail.com>
2025-12-08 04:28:52 -0800

    test filesystem functionality

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include "file_tree.hpp"
 #include "tui.hpp"
+#include "disk.hpp"
+#include "filesystem.hpp"
+#include <iostream>
 #include <ftxui/component/component.hpp>
 #include <ftxui/component/screen_interactive.hpp>
 #include <ftxui/dom/elements.hpp>
@@ -8,11 +11,37 @@ using namespace ftxui;
 
 
 int main() {
-    FileTree tree{"."};
+    // FileTree tree{"."};
+    //
+    // FileNode& root = tree.get_root_node();
+    // root.is_expanded = true;
+    //
+    // run_tui(tree);
 
-    FileNode& root = tree.get_root_node();
-    root.is_expanded = true;
+    int number_of_blocks {1024};
+    int block_size = {4096};
+    Disk disk { number_of_blocks, block_size };
 
-    run_tui(tree);
+    disk.open("disk.img");
+
+    FileSystem fs{disk, 64};
+    fs.initialize();
+    disk.close();
+
+    disk.open("disk.img");
+    fs.mount();
+    const auto& sb = fs.superblock();
+    std::cout << "total_blocks:      " << sb.total_blocks << "\n";
+    std::cout << "block_size:        " << sb.block_size << "\n";
+    std::cout << "inode_table_start: " << sb.inode_table_start << "\n";
+
+    const auto& inodes = fs.inode_table();
+    if (!inodes.empty()) {
+        std::cout << "root inode type:   "
+                  << (inodes[0].type == InodeType::DIRECTORY ? "DIRECTORY" : "OTHER")
+                  << "\n";
+        std::cout << "root index_block:  " << inodes[0].index_block << "\n";
+    }
+    disk.close();
     return 0;
 }
